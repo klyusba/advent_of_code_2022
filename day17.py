f216a49daf6a4272bb11022355b1d5b401bf0df5
Klyus Boris <klyusba@gmail.com>
2022-12-18 19:22:36 +0300

    day17 part2

--- day17.py
+++ day17.py
@@ -41,8 +41,7 @@ ROCKS = [
 
 
 def move_right(maze, rock, rock_position):
-    r = np.sum(rock[:, -1], axis=0)
-    if r:  # rock touch wall
+    if np.any(rock[:, -1]):  # rock touch wall
         return rock
     new_pos = rock.copy()
     new_pos[:, 1:] = new_pos[:, :-1]
@@ -55,8 +54,7 @@ def move_right(maze, rock, rock_position):
 
 
 def move_left(maze, rock, rock_position):
-    r = np.sum(rock[:, 0], axis=0)
-    if r:  # rock touch wall
+    if np.any(rock[:, 0]):  # rock touch wall
         return rock
     new_pos = rock.copy()
     new_pos[:, :-1] = new_pos[:, 1:]
@@ -76,15 +74,15 @@ def fix_rock(maze, rock, rock_position):
     h = rock.shape[0]
     maze[rock_position: rock_position + h, :] |= rock
 
-    y = maze.sum(axis=1)
+    y = maze[rock_position: rock_position + h, :].sum(axis=1)
     t, = np.where(y)
-    return np.max(t) + 1
+    return rock_position + np.max(t) + 1
 
 
-def main1(air_jets: str, width=7, rounds=2022):
+def model(air_jets: str, width: int, rounds: int, callback: callable = None):
     air_jets = cycle(air_jets)
     rocks = cycle(ROCKS)
-    maze = np.zeros((10000, width), dtype=bool)
+    maze = np.zeros((4 * rounds, width), dtype=bool)
     height = 0
     for _ in range(rounds):
         rock = next(rocks).copy()
@@ -98,25 +96,98 @@ def main1(air_jets: str, width=7, rounds=2022):
 
             rock_position -= 1
             if rock_position < 0 or has_intersection(maze, rock, rock_position):
-                height = fix_rock(maze, rock, rock_position+1)
+                height = max(fix_rock(maze, rock, rock_position+1), height)
                 break
+        if callback and not callback(maze[:height, :]):
+            break
+    return maze[:height, :]
 
-    return height
+
+def main1(air_jets: str, width=7, rounds=2022):
+    maze = model(air_jets, width, rounds)
+    return maze.shape[0]
 
 
 def test_part1(sample):
     assert main1(sample) == 3068
 
 
-def main2(air_jets: str, width=7, rounds=1000000000000):
-    return 0
+def encode(maze):
+    return maze.dot(2 ** np.arange(maze.shape[1]))
+
+
+def find_pattern(maze):
+    x = encode(maze)
+    fft = np.fft.rfft(x, norm="ortho")
+    y = fft.real ** 2 + fft.imag ** 2
+    c = np.fft.irfft(y, norm="ortho")
+    c[:3] = 0
+    period = np.argmax(c[:len(c)//2])
+
+    # find start of repetition by bisection
+    lower, upper = 0, maze.shape[0]-1
+    while lower < upper:
+        idx = lower + (upper - lower) // 2
+        if np.all(x[idx:idx+period] == x[idx+period:idx+2*period]):
+            upper = idx
+        else:
+            lower = idx + 1
+    return x[lower:lower+period]
+
+
+class Monitor:
+    def __init__(self, pattern):
+        self.pattern = pattern
+        self._counter = 0
+        self._first = 0
+        self._second = 0
+        self.profile = []
+
+    def __call__(self, maze):
+        self._counter += 1
+        h, n = maze.shape[0], self.pattern.shape[0]
+        self.profile.append(h)
+        if h < n:
+            return True
+        x = encode(maze)
+        if np.all(x[-n:] == self.pattern):
+            if not self._first:
+                self._first = self._counter
+                return True
+            else:
+                self._second = self._counter
+                return False
+        else:
+            return True
+
+    def get_period(self):
+        p = self._second - self._first
+        return p, self._first - p
+
+
+def main2(air_jets: str, width=7, rounds=1_000_000_000_000):
+    period = len(air_jets) * len(ROCKS)
+    maze = model(air_jets, width, period)
+    pattern = find_pattern(maze)
+
+    m = Monitor(pattern)
+    model(air_jets, width, period, m)
+    period, start = m.get_period()
+    d, r = divmod(rounds - start, period)
+    return m.profile[start + r - 1] + d * pattern.shape[0]
 
 
 def test_part2(sample):
     assert main2(sample) == 1514285714288
 
 
+def test_part2_2(sample):
+    assert main2(sample, rounds=2022) == 3068
+    assert main2(sample, rounds=3000) == main1(sample, rounds=3000)
+    assert main2(sample, rounds=5000) == main1(sample, rounds=5000)
+
+
 if __name__ == "__main__":
     data = read('input.txt')
-    print(main1(data))
-    # print(main2(data))
+    # print(main1(data))
+    print(main2(data))
